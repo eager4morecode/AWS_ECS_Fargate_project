8aa017792c42782f7a291906e40607ad0f9fb2a6
eager4morecode <115359627+eager4morecode@users.noreply.github.com>
2025-12-11 10:19:50 -0500

    Add variables for network module configuration

---
+++ infra/modules/network/variables.tf
@@ -0,0 +1,25 @@
+variable "project_name" {
+  description = "Short name for tagging and resource naming"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "public_subnet_cidrs" {
+  description = "List of CIDR blocks for public subnets"
+  type        = list(string)
+}
+
+variable "private_subnet_cidrs" {
+  description = "List of CIDR blocks for private subnets"
+  type        = list(string)
+}
+
+variable "azs" {
+  description = "Availability zones to use"
+  type        = list(string)
+  default     = []
+}
