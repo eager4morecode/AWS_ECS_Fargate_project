0963744c077282c6f75bf05a087f72d49ebeae7f
eager4morecode <115359627+eager4morecode@users.noreply.github.com>
2025-12-11 10:48:20 -0500

    Add AWS ECR repository resource configuration

---
+++ infra/modules/ecr/main.tf
@@ -0,0 +1,13 @@
+resource "aws_ecr_repository" "this" {
+  name = var.project_name
+
+  image_tag_mutability = var.image_tag_mutability
+
+  image_scanning_configuration {
+    scan_on_push = var.image_scan_on_push
+  }
+
+  encryption_configuration {
+    encryption_type = "AES256"
+  }
+}
